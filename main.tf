6975fc6bda34cdbb4495e6b84d6484759266774c
lomar92 <amarlojo@gmail.com>
2021-10-29 08:06:36 +0200

    prefix

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 module "dog-webapp" {
   source        = "app.terraform.io/amarlojo-training/dog-webapp/aws"
   version       = "1.0.0"
-  
+  prefix        = var.prefix
   instance_type = var.instance_type
   region        = var.region
 }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  }
+}
 
 variable "instance_type" {
   description = "EC2 Instance Typ definieren"
