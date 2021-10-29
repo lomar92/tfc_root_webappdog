225f9278c2f640f131db0dc62baface7ca29332c
lomar92 <amarlojo@gmail.com>
2021-10-29 08:04:04 +0200

    prefix

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
-variable "prefix" {
-  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
 
-}
 
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
